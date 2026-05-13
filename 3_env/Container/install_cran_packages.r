2ca7062805b57327c4896dc59fc5d0999e6731bf
Simon Holzinger <simon.holzinger@lit.eu>
2026-05-13 13:50:21 +0200

    Update environment information

--- 3_env/Container/install_cran_packages.r
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env R
-
-# installs packages from cran listed in packages_cran.csv and from bioconductor in packages_bioc.csv
-cran_pkgs <- read.delim("packages_cran.csv", sep="\n", header=FALSE)[["V1"]]
-
-install.packages(cran_pkgs, repos = "https://packages.othr.de/cran/")
